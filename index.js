b151fc9cdbc35dda216d07caeef88d86c31d634c
juan_cano <juanecheverricano15234@gmail.com>
2026-03-10 17:26:12 -0500

    section nosotros terminada

--- index.js
+++ index.js
@@ -19,16 +19,20 @@ btn_imagen_siguiente.addEventListener("click", () => {
 });
 
 const galeria_proyectos = document.querySelectorAll(".galeria_proyectos");
-const card_servicio = document.querySelectorAll(".card-servicio")
-const span_proyectos = document.querySelectorAll(".card_face span")
-const card_face = document.querySelectorAll(".card_face")
-const card_back = document.querySelectorAll(".card_back")
-let index_proyecto = 1
+const card_servicio = document.querySelectorAll(".card-servicio");
+
+const span_proyectos_face = document.querySelectorAll(".card_face span");
+const card_face = document.querySelectorAll(".card_face");
+
+const card_back = document.querySelectorAll(".card_back");
+const span_proyectos_back = document.querySelectorAll(".card_back span");
+const titulo_proyectos_back = document.querySelectorAll(".card_back h2");
+
+let index_proyecto = 1;
 
 function cambiar_imagen() {
   galeria_proyectos.forEach((galeria) => {
-    
-    if (index_proyecto == 4) {
+    if (galeria.scrollLeft + galeria.clientWidth >= galeria.scrollWidth) {
       galeria.scrollLeft = 0;
       index_proyecto = 1;
     } else {
@@ -38,17 +42,45 @@ function cambiar_imagen() {
   });
 }
 
-span_proyectos.forEach((span, index) => {
+span_proyectos_face.forEach((span, index) => {
   span.addEventListener("click", () => {
-    
-      card_servicio[index].classList.toggle("flip")
-      clearInterval(intervalo)
-    })
-})
+    card_servicio[index].classList.toggle("flip");
+    clearInterval(intervalo);
 
-const titulos_card_proyectos = document.querySelectorAll(".titulos_card")
+    if (index_proyecto == 1) {
+      card_back[index].style.backgroundImage =
+        "url('imagenes/imagenes-proyectos-comerciales/proyecto-1.png";
+      titulo_proyectos_back[index].textContent = "CONSULTORIO";
+    } else if (index_proyecto == 4) {
+      card_back[index].style.backgroundImage =
+        "url('imagenes/imagenes-proyectos-comerciales/proyecto-2.png";
+      titulo_proyectos_back[index].textContent = "CONSULTORIO";
+    } else if (index_proyecto == 7) {
+      card_back[index].style.backgroundImage =
+        "url('imagenes/imagenes-proyectos-comerciales/proyecto-3.png";
+      titulo_proyectos_back[index].textContent = "CONSULTORIO";
+    } else if (index_proyecto == 10) {
+      card_back[index].style.backgroundImage =
+        "url('imagenes/imagenes-proyectos-comerciales/proyecto-4.png";
+      titulo_proyectos_back[index].textContent = "CONSULTORIO";
+    }
+  });
+});
+
+span_proyectos_back.forEach((span, index) => {
+  span.addEventListener("click", () => {
+    card_servicio[index].classList.toggle("flip");
+    clearInterval(intervalo);
+  });
+});
+
+const titulos_card_proyectos = document.querySelectorAll(".titulos_card");
 let intervalo = setInterval(cambiar_imagen, 4000);
 titulos_card_proyectos.forEach((titulo_card) => {
-    titulo_card.addEventListener("mouseenter", () => {clearInterval(intervalo)})
-    titulo_card.addEventListener("mouseleave", () => {intervalo = setInterval(cambiar_imagen, 4000)})
-})
+  titulo_card.addEventListener("mouseenter", () => {
+    clearInterval(intervalo);
+  });
+  titulo_card.addEventListener("mouseleave", () => {
+    intervalo = setInterval(cambiar_imagen, 4000);
+  });
+});
